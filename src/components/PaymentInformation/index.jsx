72801204b343ec4ff9872e5d42def3a9c9ed6399
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-08-29 19:48:06 -0300

    feat: blocks user from accessing payment if registration has not been completed

--- src/components/PaymentInformation/index.jsx
+++ src/components/PaymentInformation/index.jsx
@@ -4,18 +4,22 @@ import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
 import { useContext } from 'react';
 import { PaymentContext } from '../../contexts/PaymentContext';
-import FinishPayment from './FinishPayment.jsx';
+import FinishPayment from './FinishPayment';
 import TicketSelection from './TicketSelection';
+import useEnrollment from '../../hooks/api/useEnrollment';
+import UnauthorizedAccessMessage from '../UnauthorizedAccessMessage/index.jsx';
 
 export default function PaymentInformation() {
   const { reservationData } = useContext(PaymentContext);
+  const { enrollment } = useEnrollment();
   const content = reservationData ? <FinishPayment reservation={reservationData}/> : <TicketSelection />;
+  const unauthorizedMessage = 'Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso';
 
   return (
     <>
       <Title variant="h4">Ingresso e Pagamento</Title>
       <MuiPickersUtilsProvider utils={DateFnsUtils}></MuiPickersUtilsProvider>
-      {content}
+      {enrollment ? content : <UnauthorizedAccessMessage text={unauthorizedMessage}/>}
     </>
   );
 }
@@ -23,3 +27,4 @@ export default function PaymentInformation() {
 const Title = styled(Typography)`
   margin-bottom: 20px !important;
 `;
+
