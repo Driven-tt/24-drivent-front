73b1aabd87bbdeaf7c3655c7dc73b1ea813a33a9
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-09-04 14:29:20 -0300

    fix: usePayment hook

--- src/hooks/api/usePayment.js
+++ src/hooks/api/usePayment.js
@@ -2,17 +2,22 @@ import useAsync from '../useAsync';
 import useToken from '../useToken';
 
 import * as paymentApi from '../../services/paymentApi';
+import { useContext } from 'react';
+import UserContext from '../../contexts/UserContext.js';
 
 export default function usePayment() {
   const token = useToken();
+  const { userData } = useContext(UserContext);
 
   const {
+    data: payment,
     loading: paymentLoading,
     error: paymentError,
     act: getPayment
-  } = useAsync((data) => paymentApi.get(data, token), false);
+  } = useAsync(() => paymentApi.get(userData.user.id, token));
 
   return {
+    payment,
     paymentLoading,
     paymentError,
     getPayment
